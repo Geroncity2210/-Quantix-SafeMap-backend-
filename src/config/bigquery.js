7a5ee5c6c8e8eefb7ad9fbdd1cd03db4684fd68d
Geroncity2210 <juanpablomorenopatarroyo1@gmail.com>
2026-04-29 07:30:15 -0500

    bigquery adjustments

--- src/config/bigquery.js
+++ src/config/bigquery.js
@@ -3,7 +3,10 @@ const { BigQuery } = require('@google-cloud/bigquery');
 
 const bigquery = new BigQuery({
   projectId: process.env.BIGQUERY_PROJECT_ID,
-  keyFilename: JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS)
+  credentials: {
+    client_email: process.env.GCP_CLIENT_EMAIL,
+    private_key: process.env.GCP_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  }
 });
 
 module.exports = bigquery;
